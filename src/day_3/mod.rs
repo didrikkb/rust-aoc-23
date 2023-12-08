0fea0a0541ff9ac5e7f607557bf113432b893baf
didrikkb <didrik.bjerk@live.no>
2023-12-08 17:25:56 +0000

    Day 3 cleaning

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -12,70 +12,14 @@ struct PartNumber {
 struct Symbol {
     y: i32,
     x: i32,
+    sym: u8,
 }
 
 pub fn part_1(input: Vec<Rc<str>>) -> i32 {
-    let part_nums = input
-        .iter()
-        .enumerate()
-        .flat_map(|(y, line)| {
-            line.as_bytes()
-                .iter()
-                .enumerate()
-                .filter_map(move |(x, &num)| match num {
-                    b'0'..=b'9' => Some((x as i32, y as i32, num - b'0')),
-                    _ => None,
-                })
-        })
-        .fold(Vec::new(), |mut v: Vec<PartNumber>, (x, y, num)| {
-            match v.last_mut() {
-                Some(prev) if prev.x1 + 1 == x => {
-                    prev.x1 += 1;
-                    prev.value = prev.value * 10 + num as i64;
-                }
-                _ => {
-                    v.push(PartNumber {
-                        value: num as i64,
-                        y: y,
-                        x0: x,
-                        x1: x,
-                    });
-                }
-            }
-
-            return v;
-        });
-
-    let symbols = input
-        .iter()
-        .enumerate()
-        .flat_map(|(y, line)| {
-            line.as_bytes()
-                .iter()
-                .enumerate()
-                .filter_map(move |(x, &sym)| {
-                    if sym != b'.' && (sym < b'0' || sym > b'9') {
-                        return Some(Symbol {
-                            x: x as i32,
-                            y: y as i32,
-                        });
-                    }
-                    return None;
-                })
-        })
-        .collect::<Vec<Symbol>>();
-
+    let part_nums = find_parts(input.clone());
+    let symbols = find_symbols(input);
+    let dirs = search_directions();
     let mut sum = 0;
-    let dirs = [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, 1),
-        (1, 0),
-        (1, -1),
-    ];
 
     for symbol in symbols {
         for part in &part_nums {
@@ -95,7 +39,41 @@ pub fn part_1(input: Vec<Rc<str>>) -> i32 {
 }
 
 pub fn part_2(input: Vec<Rc<str>>) -> i32 {
-    let part_nums = input
+    let part_nums = find_parts(input.clone());
+    let symbols: Vec<Symbol> = find_symbols(input)
+        .into_iter()
+        .filter(|symbol| symbol.sym == b'*')
+        .collect();
+    let dirs = search_directions();
+    let mut sum = 0;
+
+    for symbol in symbols {
+        let mut count = 0;
+        let mut temp_sum = 1;
+
+        for part in &part_nums {
+            if (symbol.y - part.y).abs() > 1 {
+                continue;
+            }
+            for (y, x) in dirs {
+                if symbol.y + y == part.y && symbol.x + x >= part.x0 && symbol.x + x <= part.x1 {
+                    temp_sum *= part.value;
+                    count += 1;
+                    break;
+                }
+            }
+        }
+
+        if count == 2 {
+            sum += temp_sum;
+        }
+    }
+
+    return sum as i32;
+}
+
+fn find_parts(input: Vec<Rc<str>>) -> Vec<PartNumber> {
+    return input
         .iter()
         .enumerate()
         .flat_map(|(y, line)| {
@@ -125,8 +103,10 @@ pub fn part_2(input: Vec<Rc<str>>) -> i32 {
 
             return v;
         });
+}
 
-    let symbols = input
+fn find_symbols(input: Vec<Rc<str>>) -> Vec<Symbol> {
+    return input
         .iter()
         .enumerate()
         .flat_map(|(y, line)| {
@@ -134,18 +114,21 @@ pub fn part_2(input: Vec<Rc<str>>) -> i32 {
                 .iter()
                 .enumerate()
                 .filter_map(move |(x, &sym)| {
-                    if sym == b'*' {
+                    if sym != b'.' && (sym < b'0' || sym > b'9') {
                         return Some(Symbol {
                             x: x as i32,
                             y: y as i32,
+                            sym: sym,
                         });
                     }
                     return None;
                 })
         })
         .collect::<Vec<Symbol>>();
+}
 
-    let dirs = [
+fn search_directions() -> [(i32, i32); 8] {
+    return [
         (-1, -1),
         (-1, 0),
         (-1, 1),
@@ -155,30 +138,6 @@ pub fn part_2(input: Vec<Rc<str>>) -> i32 {
         (1, 0),
         (1, -1),
     ];
-
-    let mut sum = 0;
-
-    for symbol in symbols {
-        let mut count = 0;
-        let mut temp_sum = 1;
-        for part in &part_nums {
-            if (symbol.y - part.y).abs() > 1 {
-                continue;
-            }
-            for (y, x) in dirs {
-                if symbol.y + y == part.y && symbol.x + x >= part.x0 && symbol.x + x <= part.x1 {
-                    temp_sum *= part.value;
-                    count += 1;
-                    break;
-                }
-            }
-        }
-        if count == 2 {
-            sum += temp_sum;
-        }
-    }
-
-    return sum as i32;
 }
 
 #[test]
